b7beba5d7ee7ea9bbcbd3fb9a4cd9631da1e8555
kehuo <hkhuoke@hotmail.com>
2020-09-25 00:16:47 +0800

    fix bug for misc/about_me

--- core/misc/resource.py
+++ core/misc/resource.py
@@ -5,11 +5,11 @@
 
 from flask_restful import Resource, reqparse
 from common.utils import encoding_resp_utf8
-from core.auth.logics.current_user import get_current_user
+from core.misc.logics.about_me import get_about_me_data
 
 
 class AboutMe(Resource):
     def get(self):
-        res = get_current_user()
+        res = get_about_me_data()
 
         return encoding_resp_utf8(res)
